d16a4a6a10ea742291cc73d6093131428ead85f4
Lucio <luciocarreras@gmail.com>
2017-05-01 19:22:21 +0200

    added logger

---
+++ src/GUI/Player/GUI_Logger.cpp
@@ -0,0 +1,135 @@
+/* GUI_Logger.cpp */
+
+/* Copyright (C) 2011-2017  Lucio Carreras
+ *
+ * This file is part of sayonara player
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
+#include "GUI_Logger.h"
+#include "GUI/Player/ui_GUI_Logger.h"
+#include "Helper/Logger/Logger.h"
+#include "Helper/Language.h"
+#include "Helper/Message/Message.h"
+
+#include <QStringList>
+#include <QTextEdit>
+#include <QThread>
+#include <QFileDialog>
+#include <QFile>
+#include <QDir>
+
+static LogObject log_object;
+
+LogObject::LogObject(QObject* parent) :
+	QObject(parent),
+	LogListener()
+{}
+
+LogObject::~LogObject() {}
+
+void LogObject::add_log_line(const QString& str)
+{
+	emit sig_new_log(str);
+}
+
+
+GUI_Logger::GUI_Logger(QWidget *parent) :
+	SayonaraWidget(parent)
+{
+	connect(&log_object, &LogObject::sig_new_log,
+			this, &GUI_Logger::log_ready, Qt::QueuedConnection);
+}
+
+GUI_Logger::~GUI_Logger()
+{
+    if(ui){
+		delete ui; ui=nullptr;
+    }
+}
+
+void GUI_Logger::init_ui()
+{
+	if(ui){
+		return;
+	}
+
+	ui = new Ui::GUI_Logger;
+	ui->setupUi(this);
+
+	for(const QString& line : _buffer) {
+		ui->te_log->appendPlainText(line);
+	}
+
+	_buffer.clear();
+
+	connect(ui->btn_close, &QPushButton::clicked, this, &QWidget::close);
+	connect(ui->btn_save, &QPushButton::clicked, this, &GUI_Logger::save_clicked);
+}
+
+void GUI_Logger::language_changed()
+{
+	if(ui){
+		ui->retranslateUi(this);
+	}
+}
+
+
+LogListener* GUI_Logger::get_log_listener()
+{
+	return &log_object;
+}
+
+
+void GUI_Logger::log_ready(const QString& str)
+{
+	if(!ui){
+		_buffer << str;
+	}
+
+	else {
+		ui->te_log->appendPlainText(str);
+	}
+}
+
+void GUI_Logger::save_clicked()
+{
+	QString filename = QFileDialog::getSaveFileName(
+						   this,
+						   Lang::get(Lang::SaveAs),
+						   QDir::homePath(), "*.log");
+
+	QFile f(filename);
+	bool is_open = f.open(QFile::WriteOnly);
+	if(is_open){
+		f.write(ui->te_log->toPlainText().toUtf8());
+		f.close();
+	}
+
+	else {
+		Message::warning(tr("Cannot open file") + " " + filename);
+	}
+}
+
+
+void GUI_Logger::showEvent(QShowEvent* e)
+{
+	init_ui();
+
+	QWidget::showEvent(e);
+}
+
---
+++ src/GUI/Player/GUI_Logger.h
@@ -0,0 +1,81 @@
+/* GUI_Logger.h */
+
+/* Copyright (C) 2011-2017  Lucio Carreras
+ *
+ * This file is part of sayonara player
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
+#ifndef GUI_LOGGER_H
+#define GUI_LOGGER_H
+
+#include "Helper/Logger/LogListener.h"
+#include "GUI/Helper/SayonaraWidget/SayonaraWidget.h"
+
+#include <QStringList>
+#include <QWidget>
+
+class QShowEvent;
+namespace Ui
+{
+    class GUI_Logger;
+}
+
+class LogObject :
+		public QObject,
+		public LogListener
+{
+		Q_OBJECT
+
+	signals:
+		void sig_new_log(const QString& str);
+
+	public:
+		explicit LogObject(QObject* parent=nullptr);
+		~LogObject();
+
+		void add_log_line(const QString& str) override;
+};
+
+
+class GUI_Logger :
+		public SayonaraWidget
+{
+	Q_OBJECT
+
+    public:
+		explicit GUI_Logger(QWidget *parent = 0);
+		~GUI_Logger();
+
+		LogListener* get_log_listener();
+
+    protected:
+		void showEvent(QShowEvent* e) override;
+
+    private:
+		Ui::GUI_Logger*		ui=nullptr;
+		QStringList			_buffer;
+
+		void init_ui();
+
+	private slots:
+		void language_changed() override;
+		void log_ready(const QString& str);
+		void save_clicked();
+};
+
+#endif // GUI_LOGGER_H
